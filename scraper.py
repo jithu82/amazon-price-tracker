3bcffd9edb20256b0d6733d516b4acd2a3a35f5d
jithu <jithendrameruga@gmail.com>
2026-06-01 15:24:06 +0530

    when fetched price the timestamp and price will be saved to database no matter how many products

--- scraper.py
+++ scraper.py
@@ -1,9 +1,15 @@
 import bs4,requests
 import time
+headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "Accept-Language": "en-IN,en;q=0.9",  # Requests the page with Indian regional formatting
+        "Accept-Encoding": "gzip, deflate, br",
+        "Referer": "https://google.com"
+    }
 
 def fetch_price(url):
     time.sleep(2)
-    response = requests.get(url)
+    response = requests.get(url,headers=headers)
     soup = bs4.BeautifulSoup(response.text,"html.parser")
     price =soup.select_one('#apex-pricetopay-accessibility-label').text
     return price.split()[0]
--- user_interface.py
+++ user_interface.py
@@ -1,7 +1,9 @@
 import scraper
+import datetime
 import sqlite3
 conn = sqlite3.connect("amazon_products_data.db",isolation_level=None)
 conn.execute("CREATE TABLE IF NOT EXISTS products(product_name TEXT,product_url TEXT)STRICT")
+conn.execute("CREATE TABLE IF NOT EXISTS prices(product_id INTEGER,price TEXT,timestamp TEXT,FOREIGN KEY(product_id) REFERENCES products(rowid))STRICT")
 def add_product():
     product_name = None
     while (product_name==None):
@@ -16,19 +18,37 @@ def show_products():
         print(f"{i+1}.",product[0])
 def price_of_single_product():
     product_name = input("enter product name:")
+    row_id = conn.execute("SELECT rowid FROM PRODUCTS WHERE product_name = ?",[product_name]).fetchone()
     url = conn.execute("SELECT product_url FROM PRODUCTS WHERE product_name = ?",[product_name]).fetchone()
     if url[0] is None:
         print("no such product")
     else:
         price = scraper.fetch_price(url[0])
-        print(f"{product_name} {price[0:-3]}")
+        print(f"{product_name} {price}")
+    time_now = datetime.datetime.now()
+    formated_time = datetime.datetime.strftime(time_now,"%d/%m/%Y %H:%M:%S")
+    conn.execute("INSERT INTO PRICES VALUES(?,?,?)",(row_id[0],price,formated_time))
+
 def delete_product():
     product_name = input("enter product name:")
     conn.execute("DELETE FROM PRODUCTS WHERE product_name = ?",(product_name,))
     print("product deleted from the data base")
+def price_of_all_products():
+    data = conn.execute("SELECT rowid,product_url FROM PRODUCTS").fetchall()
+    for i in data :
+        row_id = i[0]
+        product_url = i[1]
+        price = scraper.fetch_price(product_url)
+        print(price)
+        time_now = datetime.datetime.now()
+        formated_time = datetime.datetime.strftime(time_now,"%d/%m/%Y %H:%M:%S")
+        conn.execute("INSERT INTO PRICES VALUES(?,?,?)",(row_id,price,formated_time))
+
+
+
 while (True):
     print("\nMAIN MENU")
-    choice = input("1.add product\n2.show all products \n3.price of product\n4.delete product\n5.exit\nEnter choice:")
+    choice = input("1.add product\n2.show all products \n3.price of product\n4.delete product\n5.price of all products\n6.exit\nEnter choice:")
     match choice:
         case "1":
             add_product()
@@ -39,5 +59,7 @@ while (True):
         case "4":
             delete_product()
         case "5":
+            price_of_all_products()
+        case "6":
             print("quitting program")
             break
